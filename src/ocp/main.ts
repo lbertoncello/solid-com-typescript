e4b0c80af572e95f7f8db2ef184ebe8038eb1c6b
lbertoncello <lucas_b_andrade@hotmail.com>
2021-08-25 19:02:57 -0300

    aula 1 sobre srp

---
+++ src/ocp/main.ts
@@ -0,0 +1,29 @@
+import { Messaging } from './services/messasing';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camiseta', 49.9312));
+shoppingCart.addItem(new Product('Caderno', 9.9661));
+shoppingCart.addItem(new Product('Lápis', 1.59));
+
+console.log(shoppingCart.items);
+console.log(order.orderStatus);
+console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
+
+order.checkout();
+
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+console.log(order.orderStatus);
