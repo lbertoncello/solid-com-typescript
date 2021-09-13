070c4311e0154c2c0991a3f6092c0e4d30d263be
lbertoncello <lucas_b_andrade@hotmail.com>
2021-09-13 19:11:27 -0300

    alterado os comentarios com a definicao do lsp

--- src/lsp/main.ts
+++ src/lsp/main.ts
@@ -2,8 +2,10 @@
  * Liskov substitution principle (Princípio da substituição de Liskov) -
  * Se ϕ(x) é uma propriedade demonstrável dos objetos x de tipo T. Então ϕ(y)
  * deve ser verdadeiro para objetos y de tipo S onde S é um subtipo de T.
+ * 
+ * SUBTIPOS DEVEM PODER SUBSTITUIR O TIPO BASE SEM ALTERAR O FUNCIONAMENTO.
  *
- * Mais simples: Subtipos precisam ser substituíveis por seus tipos de base.
+ * Mais simples: Subtipos precisam ser substituíveis substituir por seus tipos base.
  * Mais simples ainda: Se meu programa espera um Animal, algo do tipo
  * Cachorro (que herda de Animal) deve servir como qualquer outro Animal.
  *
